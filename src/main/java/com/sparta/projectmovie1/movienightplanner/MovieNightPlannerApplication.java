31689a6c52514a46c1d066d7250a1d11ed868bd1
lanyamd2 <laniangel12@gmail.com>
2023-10-12 14:34:10 +0100

    gitignore changes

---
+++ src/main/java/com/sparta/projectmovie1/movienightplanner/MovieNightPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.projectmovie1.movienightplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieNightPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieNightPlannerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/projectmovie1/movienightplanner/MovieNightPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.projectmovie1.movienightplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieNightPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
