a887640a679089def588dbde44fd9b5bdf375d07
lanyamd2 <laniangel12@gmail.com>
2023-10-20 09:50:36 +0100

    added modal form to profile pages

--- src/main/java/com/sparta/projectmovie1/movienightplanner/webcontrollers/ProfileWebController.java
+++ src/main/java/com/sparta/projectmovie1/movienightplanner/webcontrollers/ProfileWebController.java
@@ -1,13 +1,12 @@
 package com.sparta.projectmovie1.movienightplanner.webcontrollers;
 
 import com.sparta.projectmovie1.movienightplanner.controllers.ProfileController;
+import com.sparta.projectmovie1.movienightplanner.models.MyPlanEntry;
 import com.sparta.projectmovie1.movienightplanner.models.movies.Movie;
 import com.sparta.projectmovie1.movienightplanner.models.tvshows.Series;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
 
 @Controller
@@ -25,6 +24,10 @@ public class ProfileWebController {
     private String getMovieById(Model model, @PathVariable("id") String id){
         Movie movie = profileController.getMovieById(id);
         model.addAttribute("movie",movie);
+
+        MyPlanEntry myPlanEntry = new MyPlanEntry();
+        myPlanEntry.setMovie(true);
+        model.addAttribute("myPlanEntry", myPlanEntry);
         return "movie-profile-page";
     }
 
@@ -32,8 +35,19 @@ public class ProfileWebController {
     private String getShowById(Model model, @PathVariable("id") String id){
         Series series = profileController.getSeriesById(id);
         model.addAttribute("show",series);
+
+        MyPlanEntry myPlanEntry = new MyPlanEntry();
+        myPlanEntry.setMovie(false);
+        model.addAttribute("myPlanEntry", myPlanEntry);
         return "tv-profile-page";
     }
+
+//    @PostMapping("/myplan/create")
+//    private String addProductionToMyPlan(@ModelAttribute("myPlanEntry") MyPlanEntry myPlanEntry){
+//
+//        return "my-plan";
+//    }
+
 
 
 }
